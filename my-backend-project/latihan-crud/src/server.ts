2c7a15b8a498fa240130c8a9ac5fa2a9aed2aee3
Sahrul Yoyo <111233171+sahrulyo@users.noreply.github.com>
2024-08-05 23:23:09 +0700

    fix error

--- my-backend-project/latihan-crud/src/server.ts
+++ my-backend-project/latihan-crud/src/server.ts
@@ -2,14 +2,12 @@ import express from "express";
 import db from "@/utils/database";
 import routes from "@/routes";
 import bodyParser from "body-parser";
-import "module-alias/register";
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 db();
-
 
 
 app.use(bodyParser.json());
